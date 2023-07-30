95f8a1fbe156f2f29b97b821b245a4add8293c14
GeekROS <15300555770>
2023-07-30 17:48:33 +0800

    1.0.0

--- package.sh
+++ package.sh
@@ -4,6 +4,8 @@ set -e
 
 lsb_release -a
 
+architecture=$(dpkg --print-architecture)
+
 filename=".robotchain/package.json"
 if [ ! -f "$filename" ]; then
     exit 0
@@ -38,6 +40,7 @@ echo -e "\033[32mPublishing the deb software package to the software repository 
 
 if [ ! -d "debian" ]; then
     mkdir -p debian
+    mkdir -p debian/DEBIAN
     mkdir -p debian/opt/tros/lib
     mkdir -p debian/opt/tros/share
     mkdir -p debian/opt/tros/lib
@@ -45,6 +48,7 @@ if [ ! -d "debian" ]; then
 else
     sudo rm -rf debian
     mkdir -p debian
+    mkdir -p debian/DEBIAN
     mkdir -p debian/opt/tros/lib
     mkdir -p debian/opt/tros/share
     mkdir -p debian/opt/tros/lib
@@ -67,5 +71,24 @@ sudo cp -r install/rosbridge_suite/share/* debian/opt/tros/share/
 sudo cp -r install/rosbridge_test_msgs/lib/* debian/opt/tros/lib/
 sudo cp -r install/rosbridge_test_msgs/include/* debian/opt/tros/include/
 sudo cp -r install/rosbridge_test_msgs/share/* debian/opt/tros/share/
+
+sudo touch debian/DEBIAN/control && sudo chmod +x debian/DEBIAN/control
+name_str="Package: title"
+export name_str
+sudo -E sh -c 'echo name_str >> debian/DEBIAN/control'
+version_str="Version: $version"
+export version_str
+sudo -E sh -c 'echo $version_str >> debian/DEBIAN/control'
+sudo sh -c 'echo "Maintainer: GEEKROS <admin@geekros.com>" >> debian/DEBIAN/control'
+sudo sh -c 'echo "Homepage: https://www.geekros.com" >> debian/DEBIAN/control'
+architecture_str="Architecture: $architecture"
+export architecture_str
+sudo -E sh -c 'echo $architecture_str >> debian/DEBIAN/control'
+sudo sh -c 'echo "Installed-Size: 5000" >> debian/DEBIAN/control'
+sudo sh -c 'echo "Section: utils" >> debian/DEBIAN/control'
+sudo sh -c 'echo "Depends: git" >> debian/DEBIAN/control'
+sudo sh -c 'echo "Description: robotchain and robot" >> debian/DEBIAN/control'
+
+sudo dpkg --build debian/ && dpkg-name debian.deb
 
 robotchain package
